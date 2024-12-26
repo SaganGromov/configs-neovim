d24f35c86b045ccc6212f599a25ed618d58be856
Matheus Andrade Ribeiro de Moura Horácio <matheus.andrade5488@gmail.com>
2024-12-26 04:02:27 -0300

    atualizacao em 26/12/2024 04:02:27

---
+++ configs-neovim/lua/matheus/plugins/treesitter.lua
@@ -0,0 +1,70 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  config = function()
+    -- import nvim-treesitter plugin
+    local treesitter = require("nvim-treesitter.configs")
+
+    -- configure treesitter
+    treesitter.setup({
+      -- modules (default configuration for all modules)
+      modules = {},
+      -- install parsers synchronously
+      sync_install = false,
+      -- list of parsers to ignore installing
+      ignore_install = {},
+
+      -- enable syntax highlighting
+      highlight = {
+        enable = true,
+        disable = { "tex" }, -- Disable Tree-sitter for .tex files
+      },
+      -- enable indentation
+      indent = {
+        enable = true,
+      },
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "latex",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+        "vimdoc",
+        "c",
+      },
+      -- configure incremental selection
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
+}
